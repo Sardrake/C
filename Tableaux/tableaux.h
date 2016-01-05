ccf7e54e2d40cf2213f1cb47fadcff8a6f482586
Arnaud <adeleval@sa-cim.fr>
2016-01-05 22:18:42 +0100

    Exos tableaux

---
+++ Tableaux/tableaux.h
@@ -0,0 +1,13 @@
+#ifndef TABLEAUX_H_INCLUDED
+#define TABLEAUX_H_INCLUDED
+
+
+
+#endif // TABLEAUX_H_INCLUDED
+
+int sommeTableau(int tableau[], int tailleTableau);
+double moyenneTableau(int tableau[], int tailleTableau);
+void copie(int tableauOriginal[], int tableauCopie[], int tailleTableau);
+void copie(int tableauOriginal[], int tableauCopie[], int tailleTableau);
+void maximumTableau(int tableau[], int tailleTableau, int valeurMax);
+void ordonnerTableau(int tableau[], int tailleTableau);
